e1221801c771532da82cdba27c8a40ee653531e5
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2022-03-29 17:01:21 -0300

    update

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
-initializeApp({
+const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -9,6 +10,8 @@ initializeApp({
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-});
+};
 
-export const firestore = getFirestore();
+export const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+export const analytics = getAnalytics(app);
